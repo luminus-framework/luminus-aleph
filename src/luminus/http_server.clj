afa88b5299e1e065838772ecf2d141bfe6cfbdf0
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-08 01:59:12 -0500

    API update

--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -2,19 +2,16 @@
   (:require [clojure.tools.logging :as log]
             [aleph.http :as http]))
 
-(defn start [handler init port]
-  (if @http-server
-    (log/error "HTTP server is already running!")
-    (try
+(defn start [{:keys [handler init port] :as opts}]
+  (try
       (init)
+      (log/info "HTTP server is starting on port " port)
       (http/start-server
-        handler
-        {:port port})
+       handler
+       (dissoc opts :handler :init))
       (log/info "server started on port:" port)
       (catch Throwable t
-        (log/error t (str "server failed to start on port: " port))))))
+        (log/error t (str "server failed to start on port: " port)))))
 
 (defn stop [destroy]
-  (when @http-server
-    (destroy)
-    (log/info "HTTP server stopped")))
+  (destroy))
