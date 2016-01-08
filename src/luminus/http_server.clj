129429f57cf92507814bcf3511f61c49a32c1d47
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-08 09:44:43 -0500

    updated logging

--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -5,13 +5,12 @@
 (defn start [{:keys [handler init port] :as opts}]
   (try
       (init)
-      (log/info "HTTP server is starting on port " port)
       (http/start-server
        handler
        (dissoc opts :handler :init))
-      (log/info "server started on port:" port)
+      (log/info "server started on port" port)
       (catch Throwable t
-        (log/error t (str "server failed to start on port: " port)))))
+        (log/error t (str "server failed to start on port " port)))))
 
 (defn stop [destroy]
   (destroy))
