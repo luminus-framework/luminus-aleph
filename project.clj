998abdb008eb7b25b90f19c7080eadce6edcc100
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-27 17:15:41 -0500

    changed to not use local state

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-aleph "0.1.0"
+(defproject luminus-aleph "0.1.1"
   :description "Aleph adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-alpeh"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [aleph "0.4.0"]])
+                 [aleph "0.4.1-beta4"]])
--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -4,13 +4,15 @@
 
 (defn start [{:keys [handler init port] :as opts}]
   (try
-      (init)
-      (http/start-server
-       handler
-       (dissoc opts :handler :init))
-      (log/info "server started on port" port)
-      (catch Throwable t
-        (log/error t (str "server failed to start on port " port)))))
+    (init)
+    (http/start-server
+      handler
+      (dissoc opts :handler :init))
+    (log/info "server started on port" port)
+    (catch Throwable t
+      (log/error t (str "server failed to start on port " port)))))
 
-(defn stop [destroy]
-  (destroy))
+(defn stop [server destroy]
+  (destroy)
+  (.close server)
+  (log/info "HTTP server stopped"))
