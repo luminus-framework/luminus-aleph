67cdd0f09350b77e49dd4b8c5107e282a5d0cfba
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2019-03-18 14:12:40 -0400

    bumped up versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-aleph "0.1.5"
+(defproject luminus-aleph "0.1.6"
   :description "Aleph adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-aleph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [aleph "0.4.2-alpha8"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [aleph "0.4.6"]])
