0167be77cc29cc2fbd162201d415c1db68a3f45c
Arnaud Geiser <arnaudgeiser@gmail.com>
2022-03-28 13:42:27 +0200

    Bump Aleph to 0.4.7

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.logging "0.4.1"]
-                 [aleph "0.4.6"]])
+                 [aleph "0.4.7"]])
