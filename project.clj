04ce4ed6abf3795ee2d5be844575effa5bd44093
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2022-10-15 15:55:05 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-aleph "0.2.0-SNAPSHOT"
+(defproject luminus-aleph "0.2.0"
   :description "Aleph adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-aleph"
   :license {:name "Eclipse Public License"
